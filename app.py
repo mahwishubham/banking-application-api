934dd0fa4b1321f9cd87a2fa92a50461e2fe4971
Mahwishubham <mazizi4646@email.phoenix.edu>
2022-06-29 14:06:59 -0400

    updating the old entry

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
-n = ((10 >= 5*2) and (10 <= 5*2))
-print(n)
+import psycopg
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    return "<h1> Welcome to Banking App </h1>"
+
+
+# Tasks - 1 Create a simple flask app and run it
+# Tasks 2 - connect your flask app to your postgresql database
+# Tasks 3 - Create customers, accounts, transaction table using flask - models
+conn = psycopg.connect(host="localhost", port="5432", dbname="postgres", user="postgres", password="zxcvbnm")
+
+print(conn)
